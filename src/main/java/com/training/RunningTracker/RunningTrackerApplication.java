d45008c6978532d49655cfd5254987faa469fd8e
SophiaAGD <sophiabelenkov@gmail.com>
2021-06-16 21:13:45 +0300

    first commit

---
+++ src/main/java/com/training/RunningTracker/RunningTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.training.RunningTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/RunningTracker/RunningTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.RunningTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
