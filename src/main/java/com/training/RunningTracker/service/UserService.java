b881e014d51272ad310e54757d72eb101ebd55a8
SophiaAGD <sophiabelenkov@gmail.com>
2021-07-03 20:53:34 +0300

    modified controller and service

--- src/main/java/com/training/RunningTracker/service/UserService.java
+++ src/main/java/com/training/RunningTracker/service/UserService.java
@@ -2,12 +2,14 @@ package com.training.RunningTracker.service;
 
 
 import com.training.RunningTracker.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserService {
     private UserDao userDao;
 
+    @Autowired
     private UserService(UserDao userDao){
         this.userDao = userDao;
     }
--- src/main/java/com/training/RunningTracker/сontroller/UserController.java
+++ src/main/java/com/training/RunningTracker/сontroller/UserController.java
@@ -1,10 +1,16 @@
 package com.training.RunningTracker.сontroller;
 
 import com.training.RunningTracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserController {
     private UserService userService;
 
-
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 
 }
