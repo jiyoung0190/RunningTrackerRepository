7a483cbb7ba7b54bec2f406533cb97c1c9f6f6e7
SophiaAGD <sophiabelenkov@gmail.com>
2021-07-04 19:53:29 +0300

    modified UserData class

--- src/main/java/com/training/RunningTracker/entity/UserData.java
+++ src/main/java/com/training/RunningTracker/entity/UserData.java
@@ -1,4 +1,37 @@
 package com.training.RunningTracker.entity;
 
-public class UserData {
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Component
+public class UserData { // this is the data which needs to be saved to and retrieved from the db
+    private Date date;
+    private float distance;
+    private Time time;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 }
