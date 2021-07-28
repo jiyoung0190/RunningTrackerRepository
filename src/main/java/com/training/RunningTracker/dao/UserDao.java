364024c3dd2eec1a311a0967dba0cc0576dc797d
SophiaAGD <sophiabelenkov@gmail.com>
2021-07-29 01:26:02 +0300

    updated GET_USER query

--- src/main/java/com/training/RunningTracker/dao/UserDao.java
+++ src/main/java/com/training/RunningTracker/dao/UserDao.java
@@ -14,12 +14,46 @@ public class UserDao {
     public static final String CREATE_USER = "insert into \"Users\" (users_id, username, password) values (?, ?, ?);";
     public static final String DELETE_USER = "delete from \"Users\" where username=?;";
     public static final String GET_USER = "select users_id, username, password from \"Users\" where username=? and password=?;";
-    public static final String GET_USER_BY_USERNAME = "select users_id, username, password from \"Users\" where username=?;";
     public static final String UPDATE_USER = "update \"Users\" set username=?, password =? where users_id=?;";
 
     @Autowired
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public User createUser(User newUser) {
+        User createdUser = new User();
+        ResultSet resultSet;
+        PreparedStatement statement;
+        try (Connection connection = dataSource.getConnection()) {
+
+            statement = connection.prepareStatement(CREATE_USER);
+            statement.setInt(1,
+                    newUser.getUsers_id());
+            statement.setString(2,
+                    newUser.getUsername());
+            statement.setString(3,
+                    newUser.getPassword());
+            statement.executeUpdate();
+
+
+            statement = connection.prepareStatement(GET_USER);
+            statement.setString(1,
+                    newUser.getUsername());
+            statement.setString(2,
+                    newUser.getPassword());
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                createdUser.setUsers_id(resultSet.getInt("users_id"));
+                createdUser.setUsername(resultSet.getString("username"));
+                createdUser.setPassword(resultSet.getString("password"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return createdUser;
     }
 
     public User getUser(User user) {
@@ -49,53 +83,6 @@ public class UserDao {
 
     }
 
-    public boolean deleteUser(User user) {
-        PreparedStatement statement;
-
-        try (Connection connection = dataSource.getConnection()) {
-            statement = connection.prepareStatement(DELETE_USER);
-            statement.setString(1,
-                    user.getUsername());
-            return statement.execute(); //statement.execute() returns either true (if its SELECT) or false (if nothing is retrieved)
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    public User createUser(User newUser) {
-        User createdUser = new User();
-        ResultSet resultSet;
-        PreparedStatement statement;
-        try (Connection connection = dataSource.getConnection()) {
-
-            statement = connection.prepareStatement(CREATE_USER);
-            statement.setInt(1,
-                    newUser.getUsers_id());
-            statement.setString(2,
-                    newUser.getUsername());
-            statement.setString(3,
-                    newUser.getPassword());
-            statement.executeUpdate();
-
-
-            statement = connection.prepareStatement(GET_USER_BY_USERNAME);
-            statement.setString(1,
-                    newUser.getUsername());
-            resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                createdUser.setUsers_id(resultSet.getInt("users_id"));
-                createdUser.setUsername(resultSet.getString("username"));
-                createdUser.setPassword(resultSet.getString("password"));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return createdUser;
-    }
-
     public User updateUser(User user) {
         User updatedUser = new User();
         PreparedStatement statement;
@@ -112,12 +99,15 @@ public class UserDao {
             statement.executeUpdate();
 
 
-            statement = connection.prepareStatement(GET_USER_BY_USERNAME);
+            statement = connection.prepareStatement(GET_USER);
             statement.setString(1,
                     user.getUsername());
+            statement.setString(2,
+                    user.getPassword());
             resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
+
                 updatedUser.setUsers_id(resultSet.getInt("users_id"));
                 updatedUser.setUsername(resultSet.getString("username"));
                 updatedUser.setPassword(resultSet.getString("password"));
@@ -127,6 +117,20 @@ public class UserDao {
             e.printStackTrace();
         }
         return updatedUser;
+    }
+
+    public boolean deleteUser(User user) {
+        PreparedStatement statement;
+
+        try (Connection connection = dataSource.getConnection()) {
+            statement = connection.prepareStatement(DELETE_USER);
+            statement.setString(1,
+                    user.getUsername());
+            return statement.execute(); //statement.execute() returns either true (if its SELECT) or false (if nothing is retrieved)
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
 
