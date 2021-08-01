bf31216cc5ca6b0e9a35f2c6f145b60a05b2fb9e
SophiaAGD <sophiabelenkov@gmail.com>
2021-08-01 22:02:09 +0300

    changed DAO Config

--- src/test/java/com/training/RunningTracker/RunningTrackerApplicationTests.java
+++ src/test/java/com/training/RunningTracker/RunningTrackerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.training.RunningTracker;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RunningTrackerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.training.RunningTracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RunningTrackerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
