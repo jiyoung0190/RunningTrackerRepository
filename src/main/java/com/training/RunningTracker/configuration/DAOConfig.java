bd40e90d39f637d50ec0a2196a317d35e38ea7bb
SophiaAGD <sophiabelenkov@gmail.com>
2021-08-01 23:02:00 +0300

    db config fix for heroku

--- src/main/java/com/training/RunningTracker/configuration/DAOConfig.java
+++ src/main/java/com/training/RunningTracker/configuration/DAOConfig.java
@@ -16,7 +16,7 @@ public class DAOConfig {
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
        // dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url("postgres://tnfygsgwsgziaq:1b40374c8f2029f19d2560572618e26826949700a344f0a9f7e8dabcb4ea8cc2@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d39d50ar6ebfmr");
+        dataSourceBuilder.url("jdbc:postgres://tnfygsgwsgziaq:1b40374c8f2029f19d2560572618e26826949700a344f0a9f7e8dabcb4ea8cc2@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d39d50ar6ebfmr");
         dataSourceBuilder.username("tnfygsgwsgziaq");
         dataSourceBuilder.password("1b40374c8f2029f19d2560572618e26826949700a344f0a9f7e8dabcb4ea8cc2");
         return dataSourceBuilder.build();
